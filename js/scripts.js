9e1c2fc38e13c15dbe817ef1e9e6557e442644a2
santio89 <santiolais1989@gmail.com>
2021-11-19 17:05:41 -0300

    test js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 function enlarge(){
     var img = document.getElementById("test");
 
-    if (img.className == "gallery__img gallery__img__active gallery__img__nohover"){
+    if (img.className == "gallery__img gallery__img__active"){
         img.className = "gallery__img";
     } else{
-        img.className = "gallery__img gallery__img__active gallery__img__nohover";
+        img.className = "gallery__img gallery__img__active";
     }
 }
 
+var img = document.getElementsByClassName(gallery__img);
+		
+for (var i=0; i < img.length; i++) {
+    img[i].onclick = function(){
+
+        if (img[i].className == "gallery__img gallery__img__active"){
+            img[i].className = "gallery__img";
+        } else{
+            img[i].className = "gallery__img gallery__img__active";
+        }
+        
+    }
+};
